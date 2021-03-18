ef6a6960c2b47cbc25172b09d9323af802e08f10
Kenji Katahira <kkatahira@uolinc.com>
2021-03-17 22:22:02 -0300

    Frontend - Creating Home component

--- frontend/blogaralho/src/components/Home.vue
+++ frontend/blogaralho/src/components/Home.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
+    <div class="home">
+        <h1>{{ title }}</h1>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'HelloWorld',
+    name: 'Home',
     data () {
         return {
-            msg: 'Welcome to Your Vue.js App'
+            title: 'Blogaralho'
         }
     }
 }
